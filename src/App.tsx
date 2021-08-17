47ffaabd655fad88b279208869c74e78a4cf1025
StarsIdea <director_9414@outlook.com>
2021-08-17 09:37:45 +0800

    added grid square

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GridSquare from './components/GridSquare'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Tetris Redux</h1>
       </header>
+      <GridSquare color="1" />
     </div>
   );
 }
